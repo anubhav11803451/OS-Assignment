3221860186bc20d283924e40a9a878140ebbc8d3
Anubhav Gupta <anubhav.11803451@lpu.in>
2020-04-16 15:04:43 +0530

    commit

--- Q26.c
+++ Q26.c
@@ -13,14 +13,15 @@ int main(int arguments, char* arv[])
 {
     int file[2];
     pid_t child;
-    char Buffer[size];
-    pipe(file);
+    char Buffer[size]; // we can directly give 50 here also but,
+                        // just to make more clear i've used seprate variable
+    pipe(file); //  Pipes are useful for communication between related processes(inter-process communication).
     if( arguments != 3 ) 
     {
         printf("ERROR: Need exactly 2 parameters.\n");
         exit(1);
     }
-    int fileOpen = open(arv[1], 0);
+    int fileOpen = open(arv[1], 0); // open() system call used to Open the file for reading, writing or both
     int TargetFile = open(arv[2], O_RDWR|O_CREAT|O_APPEND, 0666);
     if (fileOpen == -1 || TargetFile == -1) 
     {
@@ -28,14 +29,15 @@ int main(int arguments, char* arv[])
         exit(1);
     }
     child = fork();
-    if (child== 0) 
+    if (child== 0) //task for the child process should be written in p==0 part.
     {
         close(file[1]);
-	while (read(file[0], Buffer, sizeof(Buffer)) > 0) 
-	{
-		write(TargetFile, Buffer, strlen(Buffer) - 1);
+	    while (read(file[0], Buffer, sizeof(Buffer)) > 0) //The read() system call reads input typed by the user via the keyboard (file descriptor 0)
+        // and stores it in the buffer (buff) which is nothing but a character array. It will read maximum of 50 bytes.
+	    {
+		    write(TargetFile, Buffer, strlen(Buffer) - 1); //write() system call is used to write to a file descriptor.
         }
-        close(file[0]);
+        close(file[0]);     // used to close open file.
         close(TargetFile);
     }
     else 
