9c96f31df4f368fb0f81da6f02cce19829836d07
Anubhav Gupta <anubhav.11803451@lpu.in>
2020-04-16 14:49:08 +0530

    commit

--- Q1.c
+++ Q1.c
@@ -16,9 +16,10 @@ int main()
       printf("Please enter a non-negative integer!\n");
    else
    {
-      child = fork();	
+      child = fork();   //fork() is a system call used to create a new process. 
+                        //fork() returns -1 on failure; On success it returns ‘0’ in the child process and process-id of the child in the parent process.  	
       if (child == 0)
-      {
+      {  //task for the child process should be written in p==0 part.
          while (n>0)
          {
             printf("%d \n",n);
@@ -27,7 +28,7 @@ int main()
          printf("Child Process ends\n"); 
       }
       else 
-      {
+      {  //task you want the parent to perform should be written in the else 
          printf("Parent is waiting for child to complete...\n");
          wait(NULL);
          printf("Parent Process ends\n");
